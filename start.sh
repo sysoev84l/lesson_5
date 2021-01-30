590f359591399cc84fee0ade299451579bc23be3
sysoev84l <sysoev84l@outlook.com>
2021-01-30 13:35:24 +0300

    lesson_5

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 CURRENT=`pwd`
 BASENAME=`basename "$CURRENT"`
 notify-send "start browser-sync and css preprocessors for $BASENAME" -i "/home/sysoev84l/Изображения/sticker/svg/html5.svg"
-gnome-terminal --tab -e "/bin/bash -c 'cd $CURRENT; ./css.sh'" --tab -e "/bin/bash -c 'cd $CURRENT; ./bs.sh'"
+#gnome-terminal --tab -e "/bin/bash -c 'cd $CURRENT; ./css.sh'" --tab -e "/bin/bash -c 'cd $CURRENT; ./bs.sh'"
+gnome-terminal --tab -- './css.sh'
+gnome-terminal --tab -- './bs.sh'
